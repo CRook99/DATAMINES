2d01333a59a9b64f0044c5c6b7e4ff9ba5ec756b
CJRook43 <cjrook43@gmail.com>
2024-11-22 21:24:03 -0500

    movement cleanup

--- Assets/Scripts/Entities/Player/PlayerMovement.cs
+++ Assets/Scripts/Entities/Player/PlayerMovement.cs
@@ -46,70 +46,48 @@ public class PlayerController2D : MonoBehaviour
 
     private void HandleInput()
     {
-        // Buffer jump input
         if (Input.GetButtonDown("Jump"))
-        {
             _jumpBufferCounter = jumpBufferTime;
-        }
         if (Input.GetButtonUp("Jump"))
-        {
             _jumpHeld = false;
-        }
-
-        // Track if jump is held
         if (Input.GetButton("Jump"))
-        {
             _jumpHeld = true;
-        }
     }
 
     private void CheckGround()
     {
-        // Check if player is on the ground
         _isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-
-        // Reset coyote time counter if grounded
+        
         if (_isGrounded)
-        {
             _coyoteTimeCounter = coyoteTime;
-        }
     }
 
     private void ApplyCoyoteTime()
     {
-        // Decrease coyote time if in the air
         if (!_isGrounded)
-        {
             _coyoteTimeCounter -= Time.deltaTime;
-        }
     }
 
     private void ApplyJumpBuffer()
     {
-        // Decrease jump buffer timer
         if (_jumpBufferCounter > 0)
-        {
             _jumpBufferCounter -= Time.deltaTime;
-        }
     }
 
     private void Move()
     {
-        // Get horizontal input and apply movement
         float horizontal = Input.GetAxisRaw("Horizontal");
         _rb.velocity = new Vector2(horizontal * moveSpeed, _rb.velocity.y);
     }
 
     private void HandleJump()
     {
-        // Perform jump if jump buffer and coyote time are valid
         if (_jumpBufferCounter > 0 && _coyoteTimeCounter > 0)
         {
             _rb.velocity = new Vector2(_rb.velocity.x, jumpForce);
-            _jumpBufferCounter = 0; // Reset jump buffer after jump
+            _jumpBufferCounter = 0;
         }
-
-        // Apply jump height control if jump is released
+        
         if (!_jumpHeld && _rb.velocity.y > 0)
         {
             _rb.velocity = new Vector2(_rb.velocity.x, _rb.velocity.y * jumpCutMultiplier);
