3273cc06df28085109ef8fc50a8fa8d6e4579f16
twinfantasyfan <wangstanley910@gmail.com>
2024-11-22 20:54:09 -0500

    Add Level Timer

---
+++ Assets/Timer.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour
+{
+    
+    private Image _bar;
+    private float _maxTime = 100f;
+    private float _remainingTime;
+    private float _decreaseMultiplier = 5f;
+    public delegate void DepleteEvent();
+
+    void Awake()
+    {
+        _bar = GetComponent<Image>();
+        _remainingTime = _maxTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Add speed in which time decreases 
+        _remainingTime -= _decreaseMultiplier * Time.deltaTime;
+        _bar.fillAmount = (_remainingTime / 100f);
+        
+        if (_remainingTime <= 0)
+        {
+            OnDepletion?.Invoke();
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+            AddTime(5f);
+        if (Input.GetKeyDown(KeyCode.J))
+            DecreaseTime(5f);
+    }
+
+    public void AddTime(float time)
+    {
+        if (_remainingTime + time <= _maxTime)
+        {
+            _remainingTime += time;
+        }
+        else
+        {
+            _remainingTime = _maxTime;
+        }
+    }
+
+    public void DecreaseTime(float time)
+    {
+        // something to consider; nerf decreases based on player state
+        _remainingTime -= time;
+        
+    }
+
+    public static event DepleteEvent OnDepletion;
+
+
+}
