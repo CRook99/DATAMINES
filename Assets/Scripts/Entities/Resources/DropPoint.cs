aafdfe77836af9d90f0602b3927bc6bd53683822
CJRook43 <cjrook43@gmail.com>
2024-11-24 05:35:27 -0500

    things

--- Assets/Scripts/Entities/Resources/DropPoint.cs
+++ Assets/Scripts/Entities/Resources/DropPoint.cs
@@ -80,7 +80,7 @@ namespace Entities.Resources
 
         private void FulfilRequest()
         {
-            Timer.Instance.AddTime(10f);
+            Timer.Instance.AddTime(15f);
             RequestTimer = 0f;
             timerText.enabled = false;
         }
